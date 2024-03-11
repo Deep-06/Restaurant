8bead65ab331d33bf3d1fff3f4bec2c5a582df20
Deep-06 <shikhadeep.hzp@gmail.com>
2024-03-12 00:08:10 +0530

    updated

--- backend/route/booking.js
+++ backend/route/booking.js
@@ -1,9 +1,20 @@
 const express=require('express');
-const {addBooking}=require('../controller/booking')
+const {addBooking}=require('../controller/booking');
+const {BookingModel}=require('../model/bookingModel')
 
 const bookRouter=express.Router();
 
 bookRouter.post("/add", addBooking);
+
+bookRouter.get("/", async (req,res) => {
+    try {
+        const book = await BookingModel.find()
+        res.status(200).send(book)
+    } catch (err) {
+        res.status(400).send({ "error": err })
+
+    }
+})
 
 
 module.exports = {
